c2c36c896a1041417e39a3aea3086d42de0759aa
nitinkumar2911 <nitin2911.in@gmail.com>
2025-10-28 06:46:07 +0530

    Task 1

---
+++ JAVASCRIPT/EXPERIMENT/EXPERIMENT5/script.js
@@ -0,0 +1,7 @@
+const name = prompt("Enter Your Name : ");
+console.log(name);
+alert(`Hello, ${name} Welcome on this site.`);
+document.write("<h1> Hello "+ name + ", Welcome to javascript </h1>");
+
+
+
