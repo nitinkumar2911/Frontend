3f008058112b69b8b4f2ed9210e5b3d54993eae8
nitinkumar2911 <nitin2911.in@gmail.com>
2025-10-28 06:50:01 +0530

    Task 2

--- JAVASCRIPT/EXPERIMENT/EXPERIMENT5/script.js
+++ JAVASCRIPT/EXPERIMENT/EXPERIMENT5/script.js
@@ -1,7 +1,11 @@
-const name = prompt("Enter Your Name : ");
-console.log(name);
-alert(`Hello, ${name} Welcome on this site.`);
-document.write("<h1> Hello "+ name + ", Welcome to javascript </h1>");
+let age = prompt("Enter your age:");
 
+if (age >= 18) {
+  alert("You are eligible to vote!");
+  document.write("<h1>You are eligible to vote!</h1>");
+} else {
+  alert("You are not eligible yet.");
+  document.write("<h1>You are not eligible yet. Come back when you are 18!</h1>");
+}
 
 
